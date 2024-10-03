75746ac58cc2342d37e516fcae2bcb4fb8a5b789
CarlosJavierValenzue <carlos.valenzuela.j92@gmail.com>
2024-10-03 14:26:42 +0200

    commit falta implementar crud

--- src/main/java/dao/UsuarioDAO.java
+++ src/main/java/dao/UsuarioDAO.java
@@ -1,7 +1,47 @@
 package dao;
 
-public class UsuarioDAO {
+import models.Usuario;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class UsuarioDAO implements DAO<Usuario> {
 
+    private static Connection con = null;
+
+    public UsuarioDAO (Connection c){con = c;}
+
+    @Override
+    public List<Usuario> findAll() {
+        var lista = new ArrayList<Usuario>();
+        try {
+            var st = con.createStatement();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return
+    }
+
+    @Override
+    public Usuario findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void save(Usuario usuario) {
+
+    }
+
+    @Override
+    public void update(Usuario usuario) {
+
+    }
+
+    @Override
+    public void delete(Usuario usuario) {
+
+    }
 }
